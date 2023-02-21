a6328c060d7efb10008cb2771a98dd1cb44b12ea
user name <zidanesalma080@gmail.com>
2023-02-20 16:01:28 -0800

    0-5 tasks

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entery
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
